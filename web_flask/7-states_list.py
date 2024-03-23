aafb8b72b941281226c1454c89d728f502c0ac50
0xNuru <nurudeenmyself@yahoo.com>
2024-03-23 21:46:59 +0100

    add new1 teardown

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,7 +2,7 @@
 """A script that starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
-
+from models.state import State
 app = Flask(__name__)
 
 
@@ -14,13 +14,14 @@ def close_storage(_=None):
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def get_states():
-    """display a list of all states"""
-    states = storage.all("State")
-    return render_template("7-states_list.html", table="States", states=states)
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    List all states
+    """
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
 
 
-if __name__ == '__main__':
-    # Run Flask app, listening on all available network interfaces on port 5000
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
