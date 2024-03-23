9601d0095ad7f96a9e1e36bb99c6de543a5dca00
0xNuru <nurudeenmyself@yahoo.com>
2024-03-23 14:35:36 +0100

    list of states api

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_session(exception):
+    """called automatically at the end of each request"""
+    if storage is not None:
+        storage.close()
+
+@app.route("/states_list", strict_slashes=False)
+def get_states():
+    """display a list of all states"""
+    states = storage.all("State")
+    return render_template("7-states_list.html", table="States", states=states)
+
+
+if __name__ == '__main__':
+    # Run Flask app, listening on all available network interfaces on port 5000
+    app.run(host='0.0.0.0', port=5000)
